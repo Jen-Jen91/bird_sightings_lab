ca3a923cb458b201d6f4ff57680a6a6772e7a5d6
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-30 13:58:10 +0000

    Added Create route

---
+++ client/src/helpers/request_helper.js
@@ -0,0 +1,18 @@
+const RequestHelper = function (url) {
+  this.url = url;
+};
+
+RequestHelper.prototype.get = function () {
+  return fetch(this.url)
+    .then((response) => response.json());
+};
+
+RequestHelper.prototype.delete = function (id) {
+  return fetch(`${this.url}/${id}`, {
+    method: 'DELETE'
+  })
+    .then((response) => response.json());
+};
+
+
+module.exports = RequestHelper;
